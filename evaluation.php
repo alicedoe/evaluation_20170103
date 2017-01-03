defe933d1d642b59781fcf9f687900e7a8ab9de1
aliceedn <alice.edn@gmail.com>
2017-01-03 23:00:08 +0100

    commit du jour

--- evaluation.php
+++ evaluation.php
@@ -20,9 +20,8 @@ foreach ($lines as $lineNumber => $lineContent)
 }
 if(!$_POST){
 $index = fopen("index.html","w+");
-fputs($index,"<html><head><meta http-equiv=\"content-type\" content=\"text/html; charset=utf-8\" />
-<title>evaluations du 03 janvier 2017 – ville</title><link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\">
-</head><body><h1>Evaluations du 03 Janviers 2017</h1><form action=\"evaluation.php\" method=\"POST\" >");
+fputs($index,'<html><head><meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>evaluations du 03 janvier 2017 – ville</title><link rel="stylesheet" type="text/css" href="style.css"></head><body><h1>Evaluations du 03 Janviers 2017</h1><h2>Alice Gabbana</h2><form action="evaluation.php" method="POST" >');
 
 
 
@@ -38,16 +37,16 @@ for($i=0;$i<count($tab_theme);$i++){
              {
                $t = str_replace("#", "", $tab_question["texte"][$j]);
                $nb = $j +1;
-               fputs($index,"<article>".$nb);
-               fputs($index,")".$t);
-               fputs($index,"<br>");
+               fputs($index,'<article><span>'.$nb.') '.$t.'</span>');
 
 
 
                   for($k=0;$k<count($tab_qcm["texte"]);$k++){
+                      
+                      
 
                       if ($tab_qcm["question"][$k]==$j){
-                          $t = '<ul><li><input type="checkbox" name="' . $k . '" value="' . $tab_qcm["texte"][$k] . '">' . str_replace("-", "", $tab_qcm["texte"][$k]) . '</li></ul>';
+                          $t = '<ul><li><INPUT type= "radio" id = "qcm '. $j .'" name="'. $j .'" value="' . $tab_qcm["texte"][$k] . '">' . str_replace("-", "", $tab_qcm["texte"][$k]) . '</li></ul>';
                           fputs($index, $t);
                           $test_qcm = true;
 
@@ -57,7 +56,7 @@ for($i=0;$i<count($tab_theme);$i++){
 
                   }
                   if($test_qcm==false){
-                    fputs($index, "</br><textarea rows=\"4\" name=\"' . $nb . '\" cols=\"50\"></textarea>");
+                    fputs($index, '</br><textarea onclick="myFunction(this.id)" id="'. $j .'" rows="4" name="' . $j . '" cols="50"></textarea>');
                   }
                   fputs($index, "</article>");
             }
@@ -66,15 +65,28 @@ for($i=0;$i<count($tab_theme);$i++){
       fputs($index,"</section><br>");
 }
 
-fputs($index,"<INPUT TYPE=\"submit\" NAME=\"nom\" VALUE=\"Valider\"></form></body>");
+fputs($index,'<INPUT TYPE="submit" NAME="submit" VALUE="Valider"><input name="secret" type="hidden" value=""></form><script src="javascript.js"></script></body></html>');
 }
 else{
-$resultat = fopen("resultat.xml","w+");
+    
+ $c = count($_POST);
+for($i=0;$i< $c-1;$i++){
+   if (!$_POST[$i]) { header('Location: index.html'); }
+    else {
+            
+$resultat = fopen("resultat/resultat.xml","w+");
+    $_xml = '<?xml version="1.0" encoding="UTF-8"?>';
+    fwrite($resultat, $_xml);
+  fputs($resultat, '<Réponses>');  
+for($i=0;$i<count($tab_question["texte"]);$i++){
+   $nb= $i+1;
+   fputs($resultat, '<Question>' . $nb . ' : ' . rtrim($_POST[$i]) . '</Question>');
+ }
+    fputs($resultat, '</Réponses>');
+ fclose($file);
+ echo "Le fichier résultat à bien été créé.  <a href=\"resultat/resultat.xml\">View the XML.</a>";
+        
+    }
+ }
 
-  for($i=0;$i<count($tab_question["texte"]);$i++){
-    $nb= $i+1;
-    fputs($resultat, "<Question".$nb.">")
-    $rep = $_POST[$k];
-    fputs($resultat,$rep);
-  }
 }
---
+++ javascript.js
@@ -0,0 +1,18 @@
+//function myFunction(id) {
+//    var date = new Date();
+//    var question = parseInt(id);
+//    
+//    var tableau_heure[id] = date;
+//    
+//    alert("Hello! I am an alert box!");
+//    
+//    
+//}
+//
+//JSarray = new Array();
+//JSarray[0] = "you";
+//JSarray[1] = "sure";
+//JSarray[2] = "do";
+//JSarray[3] = "stink";
+//
+//function send_value(){document.submit.secret.value = JSarray.toString();}
