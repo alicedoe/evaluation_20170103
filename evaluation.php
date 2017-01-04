42dc38d7cc93b98546a4ea568800782d66b3bdf3
aliceedn <alice.edn@gmail.com>
2017-01-04 15:59:16 +0100

    dernier commit !!!

--- evaluation.php
+++ evaluation.php
@@ -1,92 +1,103 @@
 <?php
+//ouverture du fichier questions et stockage dans la variable $fichier_questions
+$fichier_questions=file('questions.qs');
+//initialisation des 3 tableaux qui stockent le fichier de questions en thèmes/questions/choix qcm
 
-$file=fopen("questions.qs","r");
-$lines=file('questions.qs');
-foreach ($lines as $lineNumber => $lineContent)
+//boucle pour remplir chaque tableau avec le contenu du fichier question
+foreach ($fichier_questions as $lineNumber => $lineContent)
 {
-    if (substr_count($lineContent,'#')==2){
-        $tab_theme[]=$lineContent;
+    if (preg_match_all("/^\#\#/", trim($lineContent)))  //si la ligne commence par ## c'est une question on stock dans le tableau themes
+       {$tab_themes[]=$lineContent;}
+    else if(preg_match_all("/^\#/", $lineContent)) //si la ligne commence par # c'est une question on stock dans le tableau questions
+    { 
+           
+      $tab_questions["enonce_question"][]=$lineContent;
+      $tab_questions["num_theme"][]=count($tab_themes)-1;
+        
+        
     }
-    else if(substr_count($lineContent,'#')==1){
-
-      $tab_question["texte"][]=$lineContent;
-      $tab_question["theme"][]=count($tab_theme)-1;
-    }
-     if(preg_match_all("#^\-#", trim($lineContent))){
-        $tab_qcm["texte"][] = $lineContent;
-        $tab_qcm["question"][] = count($tab_question["texte"])-1;
+    else if(preg_match_all("/^\-/", $lineContent)) //si la ligne commence par - c'est un choix on stock dans le tableau qcm
+     {
+        $tab_qcm["choix"][] = $lineContent;
+        $tab_qcm["num_question"][] = count($tab_questions["enonce_question"])-1;
       }
 
 }
+//si la variable post n'existe pas (qu'on ne vient pas d'un submit) alors on affiche le formulaire
 if(!$_POST){
 $index = fopen("index.html","w+");
 fputs($index,'<html><head><meta http-equiv="content-type" content="text/html; charset=utf-8" />
 <title>evaluations du 03 janvier 2017 – ville</title><link rel="stylesheet" type="text/css" href="style.css"></head><body><h1>Evaluations du 03 Janviers 2017</h1><h2>Alice Gabbana</h2><form action="evaluation.php" method="POST" >');
-
-
-
-for($i=0;$i<count($tab_theme);$i++){
-  $t = str_replace("##", "", $tab_theme[$i]);
+    
+for ( $i=0; $i<count($tab_themes);$i++ ) {
+    
+  $t = str_replace("##", "", $tab_themes[$i]);
   $texte="<section><h3>". $t ."</h3>";
   fputs($index,$texte);
-
-
-    for($j=0;$j<count($tab_question["texte"]);$j++){
+    for ( $j=0; $j<count($tab_questions["enonce_question"]);$j++ )
+    {
+        
+        
           $test_qcm=false;
-          if ($tab_question["theme"][$j]==$i)
+          if ($tab_questions["num_theme"][$j]==$i)
              {
-               $t = str_replace("#", "", $tab_question["texte"][$j]);
+               $t = str_replace("#", "", $tab_questions["enonce_question"][$j]);
                $nb = $j +1;
+                 
                fputs($index,'<article><span>'.$nb.') '.$t.'</span>');
-
-
-
-                  for($k=0;$k<count($tab_qcm["texte"]);$k++){
+                
+                    if (in_array($j, $tab_qcm["num_question"])) {
+                                       fputs($index, '<ul>');
+                                        $ul_tag = true;        }
+                 for ( $k=0; $k<count($tab_qcm["choix"]);$k++ )
+                  {                
+                     
                       
                       
+                      if ($tab_qcm["num_question"][$k]==$j){
 
-                      if ($tab_qcm["question"][$k]==$j){
-                          $t = '<ul><li><INPUT type= "radio" id = "qcm '. $j .'" name="'. $j .'" value="' . $tab_qcm["texte"][$k] . '">' . str_replace("-", "", $tab_qcm["texte"][$k]) . '</li></ul>';
+                          $t = '<li><INPUT type= "radio" onclick="updateTime(\'h'. $j .'\')" name="'. $j .'" value="' . $tab_qcm["choix"][$k] . '">' . str_replace("-", "", $tab_qcm["choix"][$k]) . '</li>';
                           fputs($index, $t);
                           $test_qcm = true;
-
                           }
 
-
-
                   }
+                 if ($ul_tag ) {
+                   fputs($index, '</ul>');
+                    $ul_tag = false;        }
                   if($test_qcm==false){
-                    fputs($index, '</br><textarea onclick="myFunction(this.id)" id="'. $j .'" rows="4" name="' . $j . '" cols="50"></textarea>');
+                    fputs($index, '</br><textarea onkeyup="updateTime(\'h'. $j .'\')" rows="4" name="' . $j . '" cols="50"></textarea>');
                   }
+                 
                   fputs($index, "</article>");
+                 fputs($index, '<input name="h'. $j .'" type="hidden">');
+                  
             }
+        
+        
 
       }
       fputs($index,"</section><br>");
 }
 
-fputs($index,'<INPUT TYPE="submit" NAME="submit" VALUE="Valider"><input name="secret" type="hidden" value=""></form><script src="javascript.js"></script></body></html>');
+fputs($index,'<INPUT TYPE="submit" NAME="submit" VALUE="Valider"></form><script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.1.1.min.js"></script><script src="javascript.js"></script></body></html>');
 }
 else{
-    
- $c = count($_POST);
-for($i=0;$i< $c-1;$i++){
-   if (!$_POST[$i]) { header('Location: index.html'); }
-    else {
+ $c = count($_POST);    
             
 $resultat = fopen("resultat/resultat.xml","w+");
     $_xml = '<?xml version="1.0" encoding="UTF-8"?>';
     fwrite($resultat, $_xml);
-  fputs($resultat, '<Réponses>');  
-for($i=0;$i<count($tab_question["texte"]);$i++){
+  fputs($resultat, '<reponses>');  
+for($i=0;$i<count($tab_questions["enonce_question"]);$i++){
+
    $nb= $i+1;
-   fputs($resultat, '<Question>' . $nb . ' : ' . rtrim($_POST[$i]) . '</Question>');
+
+   fputs($resultat, '<question> Temps : ' . $_POST["h".$i] . ' / Question ' . $nb . ' : ' . rtrim($_POST[$i]) . '</question>');
+    fputs($resultat, $heure);
  }
-    fputs($resultat, '</Réponses>');
+    fputs($resultat, '</reponses>');
  fclose($file);
  echo "Le fichier résultat à bien été créé.  <a href=\"resultat/resultat.xml\">View the XML.</a>";
-        
-    }
- }
 
 }
--- javascript.js
+++ javascript.js
@@ -1,18 +1,5 @@
-//function myFunction(id) {
-//    var date = new Date();
-//    var question = parseInt(id);
-//    
-//    var tableau_heure[id] = date;
-//    
-//    alert("Hello! I am an alert box!");
-//    
-//    
-//}
-//
-//JSarray = new Array();
-//JSarray[0] = "you";
-//JSarray[1] = "sure";
-//JSarray[2] = "do";
-//JSarray[3] = "stink";
-//
-//function send_value(){document.submit.secret.value = JSarray.toString();}
+function updateTime(name) {
+var date = new Date();
+ var time = date.getHours() + ":" + date.getMinutes();
+   $("[name='" + name + "']").val(time);
+}
